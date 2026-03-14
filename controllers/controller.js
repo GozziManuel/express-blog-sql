bf13b9bfccb08b11fb88c75638b88a62fa82cef4
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-14 15:12:02 +0100

    function show

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,8 +18,31 @@ function home(req, res) {
 
 // Funzione Show
 function show(req, res) {
-  res.json({
-    description: `you are looking at post number `,
+  paramId = parseInt(req.params.id);
+
+  const sql = `SELECT * FROM posts WHERE id = ${paramId}`;
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Database query failed",
+      });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Pizza not found",
+      });
+    }
+    console.log(results);
+
+    res.json({
+      success: true,
+      message: "Ecco a voi il blog scelto",
+      result: results[0],
+    });
   });
 }
 
